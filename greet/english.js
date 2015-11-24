d2c6c27cc2b342baf99ed7069cdfe3d1e1104c25
BarrettNashville <ag17399@AG1719WM25212.net.ads.state.tn.us>
2015-11-24 12:59:56 -0600

    Lecture 25b: using JSON files with require()

--- greet/english.js
+++ greet/english.js
@@ -1,6 +1,9 @@
+// this will return an object directly instead of having to use module.exports, since its JSON
+var greetings = require('./greetings.json');
+
 var greet = function() {
 	
-	console.log('Hello');
+	console.log(greetings.en);
 	
 }
 
