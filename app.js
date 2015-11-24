4bd476ee8c4c66f635b8b25e4fe3cc133db4b181
BarrettNashville <ag17399@AG1719WM25212.net.ads.state.tn.us>
2015-11-24 11:47:49 -0600

    Lecture 17: JavaScript Aside - first class functions and function expressions

---
+++ app.js
@@ -0,0 +1,32 @@
+// modules: a reusable block of code whose existence does not accidentally impact other code
+
+// function statement
+function greet() {
+	
+	console.log('hi');	
+	
+}
+
+greet();
+
+// functions are first-class
+function logGreeting(fn) {
+	
+	fn();
+	
+}
+logGreeting(greet);
+
+// function expression
+var greetMe = function() {
+	console.log('Hi Tony');
+}
+greetMe();
+
+// it's first-class
+logGreeting(greetMe);
+
+// use a function expression on the fly
+logGreeting(function() {
+	console.log('Hello Tony!');
+});
