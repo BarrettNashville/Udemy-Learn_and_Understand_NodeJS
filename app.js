23931b3ff133cb6c6fcd6baa75f7003478443bb4
unknown <ag17399@AG1719WM25212.net.ads.state.tn.us>
2015-11-24 12:39:04 -0600

    Test Commit from Visual Studio Code

--- app.js
+++ app.js
@@ -1,10 +1,2 @@
-//require('./greet.js');
-
-// can't call greet() here because a module doesn't affect the code in the rest of the application
-// that is, unless you use module.exports in the module which returns a value here
-
-// you can remove .js from the require parameter if you want
-// var greet = require('./greet.js');
-
 var greet = require('./greet');
 greet();
