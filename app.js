132de56b7bc0387f362027148de3c0ee51d7a7c4
BarrettNashville <ag17399@AG1719WM25212.net.ads.state.tn.us>
2015-11-25 12:42:31 -0600

    Lecture 26a: Assigning different things to Module.Exports

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
-var greet = require('./greet');
+// returns function
+var greet = require('./greet1');
+greet();
 
-greet.english();
-greet.spanish();
+// adds greet property to module.exports
+var greet2 = require('./greet2').greet;
+greet2();
+
+// calls a function constructor 
+// and returns an object with a greet method
+var greet3 = require('./greet3');
+greet3.greet();
+
+// different object or same object?
+greet3.greeting = "Changed hello world!";
+
+var greet3b = require('./greet3');
+greet3b.greet();
+// same object
+// because Node caches Module.Exports for each file name
