b12e918038f3d7cb932ea4403232ad41cf76c8c0
unknown <ag17399@AG1719WM25212.net.ads.state.tn.us>
2015-11-30 12:30:51 -0600

    Lecture 29: Using Require with Native/Core Modules

--- app.js
+++ app.js
@@ -1,31 +1,8 @@
-// returns function
-var greet = require('./greet1');
-greet();
+var util = require('util');
+// util is a native module
+// you don't put ./ before these modules
 
-// adds greet property to module.exports
-var greet2 = require('./greet2').greet;
-greet2();
-
-// calls a function constructor 
-// and returns an object with a greet method
-var greet3 = require('./greet3');
-greet3.greet();
-
-// different object or same object?
-greet3.greeting = "Changed hello world!";
-
-var greet3b = require('./greet3');
-greet3b.greet();
-// same object
-// because Node caches Module.Exports for each file name
-
-// this time we return the function constructor
-// and call it, creating a new object each time
-// instead of returning the one object every time
-// as above in greet3
-var Greet4 = require('./greet4');
-var grtr = new Greet4();
-grtr.greet();
-
-var greet5 = require('./greet5').greet;
-greet5();
+// they include additional functionality like below
+var name = 'Barrett';
+var greeting = util.format('Hello, %s', name);
+util.log(greeting);
--- greet2.js
+++
@@ -1,4 +0,0 @@
-module.exports.greet = function() {
-	
-	console.log('Hello World!!');
-}
