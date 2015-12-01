d24636f8f3178ca23edb1e4b7dcdb1905c68b562
BarrettNashville <ag17399@AG1719WM25212.net.ads.state.tn.us>
2015-12-01 13:04:41 -0600

    Lecture 41: Using a class for our function constructor from ES6

--- app.js
+++ app.js
@@ -1,24 +1,7 @@
-var EventEmitter = require('events');
-var util = require('util');
+// update this code using ES6 classes
+'use strict';
 
-function Greetr() {
-	//this next line sends objects created with this function
-	//constructor to the EventEmitter constructor to add on
-	//additional functionality to this object directly, 
-	//instead of through the prototype.
-	//this one line of code ensures that my object created
-	//from this function constructor has everything 
-	//an object created from an event emitter would have
-	EventEmitter.call(this);
-	this.greeting = 'Hello World!';
-}
-
-util.inherits(Greetr, EventEmitter);
-
-Greetr.prototype.greet = function(data) {
-	console.log(this.greeting + ': ' + data);
-	this.emit('greet', data); 
-}
+var Greetr = require('./greetr');
 
 var greeter1 = new Greetr();
 
