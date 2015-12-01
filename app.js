8b178b3447d08e3479d945eefc796951898ce455
BarrettNashville <ag17399@AG1719WM25212.net.ads.state.tn.us>
2015-12-01 12:51:04 -0600

    Lecture 39: Calling the event emitters constructor on objects created by our constructor

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-var obj = {
-	name: 'John Doe',
-	greet: function() {
-		console.log(`Hello ${this.name}`);
-	}
+var EventEmitter = require('events');
+var util = require('util');
+
+function Greetr() {
+	//this next line sends objects created with this function
+	//constructor to the EventEmitter constructor to add on
+	//additional functionality to this object directly, 
+	//instead of through the prototype.
+	//this one line of code ensures that my object created
+	//from this function constructor has everything 
+	//an object created from an event emitter would have
+	EventEmitter.call(this);
+	this.greeting = 'Hello World!';
 }
 
-obj.greet();
-// whatever you pass to call is what the 'this' keyword will point to
-obj.greet.call({ name: 'Jane Doe'});
-// apply is the same as call except you pass parameters in an array
-// instead of a comma separated list as in call
-obj.greet.apply({ name: 'Jane Doe'});
+util.inherits(Greetr, EventEmitter);
+
+Greetr.prototype.greet = function(data) {
+	console.log(this.greeting + ': ' + data);
+	this.emit('greet', data); 
+}
+
+var greeter1 = new Greetr();
+
+greeter1.on('greet', function(data) {
+	console.log('Someone greeted!: ' + data);
+});
+
+greeter1.greet('Barrett');
