992505b47e23c807910bb9f2488a3d4149281064
BarrettNashville <ag17399@AG1719WM25212.net.ads.state.tn.us>
2015-12-01 12:26:16 -0600

    Lecture 36: Inheriting from the EventEmitter

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-var Emitter = require('events');
-var eventConfig = require('./config').events;
+var EventEmitter = require('events');
+var util = require('util');
 
-var emtr = new Emitter();
+function Greetr() {
+	this.greeting = 'Hello World!';
+}
 
-emtr.on(eventConfig.GREET, function() {
-	console.log('Somewhere, someone said hello.');	
+// any objects created from Greetr should also have access
+// to the methods and properties on the prototype property
+// of EventEmitter
+util.inherits(Greetr, EventEmitter);
+
+// you can still add additional properties to the prototype
+// of Greetr
+Greetr.prototype.greet = function(data) {
+	console.log(this.greeting + ': ' + data);
+	
+	// a method from the EventEmitter prototype
+	// we are able to pass parameters to the emit method,
+	// data in this case
+	this.emit('greet', data); 
+}
+
+var greeter1 = new Greetr();
+
+greeter1.on('greet', function(data) {
+	console.log('Someone greeted!: ' + data);
 });
 
-emtr.on(eventConfig.GREET, function() {
-	console.log('A greeting occurred!');	
-});
-
-console.log('Hello!');
-emtr.emit(eventConfig.GREET);
+greeter1.greet('Barrett');
