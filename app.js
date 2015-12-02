91c3dde23f5ebc7b051fe1359c7c977021e4f87a
BarrettNashville <ag17399@AG1719WM25212.net.ads.state.tn.us>
2015-12-02 12:06:38 -0600

    Lecture 48: ES6 Typed Arrays

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-var buf = new Buffer('Hello', 'utf8'); // create buffer with UTF8 encoding
-console.log(buf); // show contents of buffer
-console.log(buf.toString()); // show contents as string
-console.log(buf.toJSON()); // show contents as JSON
-console.log(buf[2]); // buffers act like arrays
-buf.write('wo'); // overwrite first two chars in buffer
-console.log(buf.toString()); //show new contents of buffer
+// ES6 typed arrays: using binary data in javascript
+
+var buffer = new ArrayBuffer(8); // 8 bytes
+var view = new Int32Array(buffer); // a way to deal with binary data in the buffer
+view[0] = 5;
+view[1] = 15;
+console.log(view);
