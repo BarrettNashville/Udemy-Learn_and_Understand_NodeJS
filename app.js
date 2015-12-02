5b7c226eaa7e60df02a62ad15e703fd1586aff64
BarrettNashville <ag17399@AG1719WM25212.net.ads.state.tn.us>
2015-12-02 12:11:22 -0600

    Lecture 49: JavaScript Callbacks (with parameters)

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-// ES6 typed arrays: using binary data in javascript
+function greet(callback) {
+	console.log('Hello!');
+	var data = {
+		name: 'John Doe'
+	};
+	callback(data);
+}
 
-var buffer = new ArrayBuffer(8); // 8 bytes
-var view = new Int32Array(buffer); // a way to deal with binary data in the buffer
-view[0] = 5;
-view[1] = 15;
-console.log(view);
+greet(function(data) {
+	console.log('The callback was invoked');
+	console.log(data);
+});
+
+greet(function(data) {
+	console.log('A different callback was invoked');
+	console.log(data.name);
+});
