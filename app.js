2836b530d9e225f8a5b48f767254b8591a2920f7
BarrettNashville <ag17399@AG1719WM25212.net.ads.state.tn.us>
2015-12-02 12:02:09 -0600

    Lecture 47: Understanding buffers

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
-// update this code using ES6 classes
-'use strict';
-
-var Greetr = require('./greetr');
-
-var greeter1 = new Greetr();
-
-greeter1.on('greet', function(data) {
-	console.log('Someone greeted!: ' + data);
-});
-
-greeter1.greet('Barrett');
+var buf = new Buffer('Hello', 'utf8'); // create buffer with UTF8 encoding
+console.log(buf); // show contents of buffer
+console.log(buf.toString()); // show contents as string
+console.log(buf.toJSON()); // show contents as JSON
+console.log(buf[2]); // buffers act like arrays
+buf.write('wo'); // overwrite first two chars in buffer
+console.log(buf.toString()); //show new contents of buffer
