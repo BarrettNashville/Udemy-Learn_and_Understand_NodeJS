df418f36c920d6a06fa710bc8101d222687f13b0
BarrettNashville <ag17399@AG1719WM25212.net.ads.state.tn.us>
2015-12-02 12:27:59 -0600

    Lecture 50: Files and fs; error-first callbacks

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-function greet(callback) {
-	console.log('Hello!');
-	var data = {
-		name: 'John Doe'
-	};
-	callback(data);
-}
+// get built-in fs module
+var fs = require('fs');
 
-greet(function(data) {
-	console.log('The callback was invoked');
-	console.log(data);
-});
+// read file syncrhonously
+// may be good for config file you need to read before other code should run
+var greet = fs.readFileSync(__dirname + '/greet.txt', 'utf8');
+console.log(greet);
 
-greet(function(data) {
-	console.log('A different callback was invoked');
-	console.log(data.name);
+var greet2 = fs.readFile(__dirname + '/greet.txt', 'utf8', 
+// callback function for asynchronous function call
+// Error-First Callback: callbacks that take an error object
+// as their first parameter. 
+// null if no error, otherwise will contain an object defining the error.
+// This is a standard so we know in what order to place our 
+// parameters for our callbacks. 
+function(err, data) {
+	console.log(data); // returns a buffer
 });
+// this uses buffers so it could cause performance problems
+// if a lot of people are using this code at once.
+// that puts a lot of stuff on the heap
+
+console.log('Done!');
