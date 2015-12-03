d8b572a56fc1ecc9347c61cf58c8a40db63ebd4b
BarrettNashville <ag17399@AG1719WM25212.net.ads.state.tn.us>
2015-12-03 11:48:27 -0600

    Lecture 53: Using Pipes with streams, including gzip

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
+// pipe: connecting two streams by reading from one and writing it to another
+// in Node you pipe from a Readable stream to a Writable stream
 var fs = require('fs');
+var zlib = require('zlib'); // allows us to gzip
 
-// the second argument to createReadStream is an optional options object 
-var readable = fs.createReadStream(__dirname + '/greet.txt', 
-	{ encoding: 'utf8', highWaterMark: 16 * 1024});
+var readable = fs.createReadStream(__dirname + '/greet.txt');
 	
 var writable = fs.createWriteStream(__dirname + '/greetcopy.txt');
 
-/*
-readable.on('data', function(chunk) {
-	console.log(chunk);
-});
-*/
-// the default buffer size is 64k so it only logs once if txt file less than that.
-// if file is larger than that, log is run multiple times.
+// pipe() is called from a readable stream and accepts a writable stream
+//readable.pipe(writable);
 
-readable.on('data', function(chunk) {
-	console.log(chunk.length);
-	
-	//copy contents of greet.txt in chunks to greetcopy.txt
-	//this minimizes memory usage on the server.
-	writable.write(chunk);
-});
-// this is so common in Node, there is a faster way to do it
+var compressed = fs.createWriteStream(__dirname + '/greet.txt.gz');
+
+
+// creates a "transform" stream, both readable and writable
+// this variable holds the compressed stream
+var gzip = zlib.createGzip();
+
+readable.pipe(writable);
+
+// read from text file, compress it, and write compressed contents to file
+readable.pipe(gzip).pipe(compressed);
