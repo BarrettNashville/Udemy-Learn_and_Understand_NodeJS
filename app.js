29dc1ca18352c182af74671dc9f0977c759e2ce8
BarrettNashville <ag17399@AG1719WM25212.net.ads.state.tn.us>
2015-12-03 12:28:57 -0600

    Lecture 59: Building a basic Web Server in Node

--- app.js
+++ app.js
@@ -1,23 +1,11 @@
-// pipe: connecting two streams by reading from one and writing it to another
-// in Node you pipe from a Readable stream to a Writable stream
-var fs = require('fs');
-var zlib = require('zlib'); // allows us to gzip
+var http = require('http');
 
-var readable = fs.createReadStream(__dirname + '/greet.txt');
+http.createServer(function(req, res) {
 	
-var writable = fs.createWriteStream(__dirname + '/greetcopy.txt');
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('Hello World\n');
+	
+	//this is the IP for localhost
+}).listen(1337, '127.0.0.1');
 
-// pipe() is called from a readable stream and accepts a writable stream
-//readable.pipe(writable);
-
-var compressed = fs.createWriteStream(__dirname + '/greet.txt.gz');
-
-
-// creates a "transform" stream, both readable and writable
-// this variable holds the compressed stream
-var gzip = zlib.createGzip();
-
-readable.pipe(writable);
-
-// read from text file, compress it, and write compressed contents to file
-readable.pipe(gzip).pipe(compressed);
+// accessible through: http://localhost:1337/
