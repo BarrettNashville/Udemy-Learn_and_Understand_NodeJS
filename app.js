60769480c7c50f4a10ed92014558762952ce6c42
BarrettNashville <ag17399@AG1719WM25212.net.ads.state.tn.us>
2015-12-03 12:52:37 -0600

    Lecture 60b: Outputting an html template with some custom code

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ var fs = require('fs');
 http.createServer(function(req, res) {
 	
 	res.writeHead(200, {'Content-Type': 'text/html'});
-	var html = fs.readFileSync(__dirname + '/index.htm');
+	// change buffer to a string by adding 'utf8'
+	var html = fs.readFileSync(__dirname + '/index.htm', 'utf8');
+	var message = 'Hello world...';
+	html = html.replace('{Message}', message);
 	res.end(html);
 	
 }).listen(1337, '127.0.0.1');
+
+// template: text designed to be the basis for 
+// final text or content after being processed.
+// There's usually some specific template language,
+// so the template system knows how to replace 
+// placeholders with real values.
