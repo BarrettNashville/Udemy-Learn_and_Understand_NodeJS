b8fa4eb0fd9610d4379ef68d76ce5a007de60edd
BarrettNashville <ag17399@AG1719WM25212.net.ads.state.tn.us>
2015-12-04 12:20:07 -0600

    Lecture 63: Outputting JSON

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var fs = require('fs');
 
 http.createServer(function(req, res) {
 	
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	// piping to the response stream asyncrhonously
-	// because of createReadStream
-	fs.createReadStream(__dirname + '/index.htm').pipe(res);
+	res.writeHead(200, {'Content-Type': 'application/json'});
+	var object = {
+		firstname: 'John',
+		lastname: 'Doe'
+	};
+	res.end(JSON.stringify(object));
 	
 }).listen(1337, '127.0.0.1');
