3de1317ba82c94bd7757c7630033c52602857de5
BarrettNashville <ag17399@AG1719WM25212.net.ads.state.tn.us>
2015-12-04 15:26:35 -0600

    Lecture 69: using NPM - Node Package Manager

--- app.js
+++ app.js
@@ -1,24 +1,2 @@
-var http = require('http');
-var fs = require('fs');
-
-http.createServer(function(req, res) {
-	
-	if(req.url === '/') {
-		fs.createReadStream(__dirname + '/index.htm').pipe(res);
-	}
-	
-	else if(req.url === '/api') {
-		res.writeHead(200, {'Content-Type': 'application/json'});
-		var object = {
-			firstname: 'John',
-			lastname: 'Doe'
-		};
-		res.end(JSON.stringify(object));
-	}
-	else {
-		// default 
-		res.writeHead(404);
-		res.end('404 - Nothing here');
-	}
-	
-}).listen(1337, '127.0.0.1');
+var moment = require('moment'); // looks first in node core, then in node_modules folder
+console.log(moment().format("ddd, hA"));
