be43ee801a1fd97c6d5f61c295a89c18a42f97b7
BarrettNashville <ag17399@AG1719WM25212.net.ads.state.tn.us>
2015-12-04 12:10:24 -0600

    Lecture 61a: Modifying simple http server to use streams

--- app.js
+++ app.js
@@ -4,16 +4,8 @@ var fs = require('fs');
 http.createServer(function(req, res) {
 	
 	res.writeHead(200, {'Content-Type': 'text/html'});
-	// change buffer to a string by adding 'utf8'
-	var html = fs.readFileSync(__dirname + '/index.htm', 'utf8');
-	var message = 'Hello world...';
-	html = html.replace('{Message}', message);
-	res.end(html);
+	// piping to the response stream asyncrhonously
+	// because of createReadStream
+	fs.createReadStream(__dirname + '/index.htm').pipe(res);
 	
 }).listen(1337, '127.0.0.1');
-
-// template: text designed to be the basis for 
-// final text or content after being processed.
-// There's usually some specific template language,
-// so the template system knows how to replace 
-// placeholders with real values.
