2fa305f270ff7a88f821464839b8b6a6e77638e2
BarrettNashville <ag17399@AG1719WM25212.net.ads.state.tn.us>
2015-12-04 15:54:01 -0600

    Lecture 71: devDependencies and nodemon (Node Monitor)

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
-var moment = require('moment'); // looks first in node core, then in node_modules folder
-console.log(moment().format("ddd, hA"));
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(req, res) {
+	
+	if(req.url === '/') {
+		fs.createReadStream(__dirname + '/index.htm').pipe(res);
+	}
+	
+	else if(req.url === '/json') {
+		res.writeHead(200, {'Content-Type': 'application/json'});
+		var object = {
+			firstname: 'John',
+			lastname: 'Doe'
+		};
+		res.end(JSON.stringify(object));
+	}
+	else {
+		// default 
+		res.writeHead(404);
+		res.end('404 - Nothing here');
+	}
+	
+}).listen(1337, '127.0.0.1');
