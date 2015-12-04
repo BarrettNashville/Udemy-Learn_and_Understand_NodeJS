ce4a5554768fdd9adbfda5f1b0abaaf1ecd6e626
BarrettNashville <ag17399@AG1719WM25212.net.ads.state.tn.us>
2015-12-04 14:55:20 -0600

    Lecture 64: Individually routing URLs to content

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ var fs = require('fs');
 
 http.createServer(function(req, res) {
 	
-	res.writeHead(200, {'Content-Type': 'application/json'});
-	var object = {
-		firstname: 'John',
-		lastname: 'Doe'
-	};
-	res.end(JSON.stringify(object));
+	if(req.url === '/') {
+		fs.createReadStream(__dirname + '/index.htm').pipe(res);
+	}
+	
+	else if(req.url === '/api') {
+		res.writeHead(200, {'Content-Type': 'application/json'});
+		var object = {
+			firstname: 'John',
+			lastname: 'Doe'
+		};
+		res.end(JSON.stringify(object));
+	}
+	else {
+		// default 
+		res.writeHead(404);
+		res.end('404 - Nothing here');
+	}
 	
 }).listen(1337, '127.0.0.1');
