c2f2ecd45c0ad1baf0f0dc6ef3e0b9ed54c63f05
BarrettNashville <ag17399@AG1719WM25212.net.ads.state.tn.us>
2015-12-05 17:32:07 -0600

    Lecture 74: Using routes with URL parameters in Express

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 var express = require('express');
 var app = express();
 
-// use the PORT environment variable, if it exists, or 3000 if it does not
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
-	// you don't have to specify content type or header info; 
-	// express figures that out for you
 	res.send('<html><head></head><body><h1>Hello world!!</h1></body></html>');
 });
 
+// :id is a parameter passed via the url
+// the colon ":" tells Express that id could be anything and run this function
+app.get('/person/:id', function(req, res) {
+	res.send('<html><head></head><body><h1>Person: ' + req.params.id + '</h1></body></html>');
+});
+
 app.get('/api', function(req, res) {
-	// json takes care of converting the JavaScript object to JSON
 	res.json({ firstname: 'John', lastname: 'Doe' });
 });
 
