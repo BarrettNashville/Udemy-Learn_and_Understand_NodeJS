eafd77e859ec9d2819a316e41cd93b1caa8ddc56
BarrettNashville <ag17399@AG1719WM25212.net.ads.state.tn.us>
2015-12-05 18:17:52 -0600

    Lecture 76: Using the EJS template engine

--- app.js
+++ app.js
@@ -3,24 +3,26 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
-// using some middleware to load a CSS file
-// it's typical to have a "public"" folder that is accessible via a /assets URL
-// app.use just takes a pattern and returns a function. 
-// express.static() returns a function
 app.use('/assets', express.static(__dirname + '/public'));
 
-// you can create your own middleware using app.use() as well
+// using a basic templating engine called EJS, simliar to ASP.NET web forms
+// by default express will look for the static view files inside a folder called views
+app.set('view engine', 'ejs'); 
+
 app.use('/', function(req, res, next) {
 	console.log('Request Url: ' + req.url); 
-	next(); // this just means: run the next middlware
+	next(); 
 });
 
 app.get('/', function(req, res) {
-	res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet /></head><body><h1>Hello world!!</h1></body></html>');
+	//express will go to views and look for a file named below, plus the extension above (ejs).
+	//this makes changing the view engine easier 
+	res.render('index');
 });
 
 app.get('/person/:id', function(req, res) {
-	res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet /></head><body><h1>Person: ' + req.params.id + '</h1></body></html>');
+	// the second argument passed to render is an object of URL parameters
+	res.render('person', {ID: req.params.id});
 });
 
 app.get('/api', function(req, res) {
